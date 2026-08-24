8d40cdf187f013ceddbcd0d013e6555ae47cd1da
Abdirahmanahmed2 <acmedcabdiraxmaan@gmail.com>
2026-08-24 01:48:00 -0700

    Multer schema aa soo saaray

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,11 @@ const app = express()
 
 app.use( express.json())
 
+const imagerouter = require("./model/router/organizationrouter")
+
+app.use("/allImages" , express.static("images"))
+app.use("/image" ,imagerouter)
+
 mongoose.connect("mongodb://localhost:27017/Hackathon").then(() =>{
     console.log("connnected")
 }).catch((err) =>{
---
+++ Backend/model/router/organizationrouter.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const router = express.Router();
+
+const imagecontroller = require("../controller/Organizationcontroller");
+
+const uploadiamge = require("./controller/middleware/upload");
+
+
+// CREATE
+router.post(
+    "/",
+    uploadiamge.single("image"),
+    imagecontroller.create
+);
+
+
+// GET
+router.get(
+    "/",
+    imagecontroller.get
+);
+
+
+// PUT
+router.put(
+    "/:id",
+    uploadiamge.single("image"),
+    imagecontroller.update
+);
+
+
+// DELETE
+router.delete(
+    "/:id",
+    imagecontroller.remove
+);
+
+
+module.exports = router;
