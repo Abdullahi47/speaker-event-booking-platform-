40385d9054a7c2e49cb18e3da758bf9ed377a15c
abdullahi2312 <xasancabdulaahi132@gmail.com>
2026-08-24 23:21:29 +0300

    Registered user API routes

--- Backend/index.js
+++ Backend/index.js
@@ -8,9 +8,11 @@ const app = express()
 app.use( express.json())
 
 const imagerouter = require("./model/router/organizationrouter")
+const userRouter = require("./routes/UserRouter")
 
 app.use("/allImages" , express.static("images"))
 app.use("/image" ,imagerouter)
+app.use("/users", userRouter)
 
 mongoose.connect("mongodb://localhost:27017/Hackathon").then(() =>{
     console.log("connnected")
