0b98a3910cbb1b550000632c2711e3a11733bebb
Abdirahmanahmed2 <acmedcabdiraxmaan@gmail.com>
2026-08-24 20:26:48 -0700

    added

---
+++ Backend/model/controller/middleware/payment.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const paymentRouter = require("./paymentRouter");
+
+const router = express.Router();
+
+router.use("/payment", paymentRouter);
+
+module.exports = router;
